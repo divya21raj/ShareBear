ae3cbd5de68e92174b6842178e29f46df617e46e
SimranS <simran20051998@gmail.com>
2018-10-28 20:36:12 +0530

    HomeScreen designing #1
    -Search Bar added
    -Basic scrollable list added

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -1,43 +1,97 @@
-import React, { Component } from 'react';
-import { View, Text, Platform, StyleSheet } from 'react-native';
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TextInput,
+    Platform,
+    StatusBar,
+    ScrollView,
+    Image,
+    Dimensions
+} from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons'
+import Category from './HomeScreen/Category.js'
 
-
+const { height, width } = Dimensions.get('window')
 const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-      'Double tap R on your keyboard to reload,\n' +
-      'Shake or press menu button for dev menu',
-  });
-  
-  
-export default class HomeScreen extends Component {
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+class HomeScreen extends Component {
+
+    componentWillMount() {
+        this.startHeaderHeight = 80
+        if (Platform.OS == 'android') {
+            this.startHeaderHeight = 100 + StatusBar.currentHeight
+        }
+    }
+
     render() {
-      return (
-        <View style={styles.container}>
-          <Text style={styles.welcome}>Welcome to ShareBear!</Text>
-          <Text style={styles.instructions}>Share, Bare and Care</Text>
-          <Text style={styles.instructions}>{instructions}</Text>
-        </View>
-      );
-    }  
-  }  
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+                <View style={{ flex: 1 }}>
+                    <View style={{ height: this.startHeaderHeight, backgroundColor: 'white', borderBottomWidth: 1, borderBottomColor: '#dddddd' }}>
+                        <View style={{
+                            flexDirection: 'row', padding: 10,
+                            backgroundColor: 'white', marginHorizontal: 20,
+                            shadowOffset: { width: 0, height: 0 },
+                            shadowColor: 'black',
+                            shadowOpacity: 0.2,
+                            elevation: 1,
+                            marginTop: Platform.OS == 'android' ? 30 : null
+                        }}>
+                            <Icon name="ios-search" size={20} style={{ marginRight: 10 }} />
+                            <TextInput
+                                underlineColorAndroid="transparent"
+                                placeholder="Try Me"
+                                placeholderTextColor="grey"
+                                style={{ flex: 1, fontWeight: '700', backgroundColor: 'white' }}
+                            />
+                        </View>
+                    </View>
+                    <ScrollView
+                        scrollEventThrottle={16}
+                    >
+                        <View style={{ flex: 1, backgroundColor: 'white', paddingTop: 20 }}>
+                            <Text style={{ fontSize: 24, fontWeight: '700', paddingHorizontal: 20 }}>
+                                What can we help you find?
+                            </Text>
+
+                            <View style={{ height: 130, marginTop: 20 }}>
+                                <ScrollView
+                                    horizontal={true}
+                                    showsHorizontalScrollIndicator={false}
+                                >
+                                    <Category imageUri={require('../../assets/home.jpg')}
+                                        name="Home"
+                                    />
+                                    <Category imageUri={require('../../assets/experiences.jpg')}
+                                        name="Experiences"
+                                    />
+                                    <Category imageUri={require('../../assets/restaurant.jpg')}
+                                        name="Resturant"
+                                    />
+                                </ScrollView>
+                            </View>
+                        </View>
+                    </ScrollView>
+
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
+export default HomeScreen;
 
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#F5FCFF',
-    },
-    welcome: {
-      fontSize: 20,
-      textAlign: 'center',
-      margin: 10,
-    },
-    instructions: {
-      textAlign: 'center',
-      color: '#333333',
-      marginBottom: 5,
-    },
-  });
-  
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
