c7823bcca461631c89dd68feb5a51261c68588a5
SimranS <simran20051998@gmail.com>
2018-11-09 00:55:18 +0530

    ItemCard Added

--- src/components/reusables/ItemCard.js
+++ src/components/reusables/ItemCard.js
@@ -1,30 +1,44 @@
 import React, { Component } from 'react';
-import { TouchableOpacity } from 'react-native';
+import { TouchableOpacity, Text } from 'react-native';
 
-import { Card } from 'native-base';
 import { withNavigation } from 'react-navigation';
-
-const horizontalMargin = 20;
-const slideWidth = 280;
-
-const itemWidth = slideWidth + horizontalMargin * 2;
-const itemHeight = 200;
-
-const styles = {
-    
-}
+import { Icon, Button, StyleProvider, ListItem, Left, Right, Body, Content, Thumbnail} from 'native-base';
 
 class ItemCard extends Component{
     render() {
-        const {item, navigation} = this.props;
+        const {item, rightIconName} = this.props;
         return(
             <TouchableOpacity
                 activeOpacity={0.9}
-                onPress={() => navigation.navigate('ItemDetail', {'item': item})}
+                onPress={() => this.props.navigation.navigate('ItemDetail', {'item': item})}
             >
-            
+                <ListItem avatar>
+                <Left>
+                    <Thumbnail source={item.thumbnail} />
+                </Left>
+                <Body>
+                    <Text style={styles.bodyText}>{item.title}</Text>
+                    <Text note>{item.subtitle}</Text>
+                    <Text note>{item.rating}</Text>
+                    <Text note>{item.size}</Text>
+                </Body>
+                <Right>
+                    <Button size={10} transparent>
+                        <Icon style={{ padding: 0, }} name={rightIconName} color={'black'} />
+                    </Button>
+                </Right>
+                </ListItem>
             </TouchableOpacity>
         );
+    }
+}
+
+const styles= {
+    bodyText:{
+        fontSize:18,
+        paddingBottom:5,
+        paddingTop:0,
+        fontWeight:'400'
     }
 }
 
--- src/components/screens/ItemListScreen/ItemListScreen.js
+++ src/components/screens/ItemListScreen/ItemListScreen.js
@@ -2,17 +2,74 @@ import React, { Component } from 'react';
 
 import {
     View,
-    StatusBar
-} from "react-native";
+    StatusBar,
+    TouchableOpacity, ScrollView,
+    SafeAreaView,
+} from 'react-native';
 
 // External Library imports
-import { Button, Container, H2, H3, Text, Card, List, } from 'native-base';
+import { Container, Text, Content} from 'native-base';
+import { withNavigation } from 'react-navigation';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 // Local Imports
 import ItemListScreenHeader from './ItemListScreenHeader';
-//import ItemCard from '../../reusables/ItemCard';
+import ItemCard from '../../reusables/ItemCard';
 
-export default class ItemListScreen extends Component{
+class ItemListScreen extends Component {
+    state = { items: [
+        {
+            title: 'WWE 2K18',
+            thumbnail: require('../../../assets/sr.jpg'),
+            subtitle: 'for PC',
+            rating: 4.5,
+            size: '32 GB',
+        },
+        {
+            title: 'FIFA 18',
+            thumbnail: require('../../../assets/games.jpg'),
+            subtitle: 'for PC',
+            rating: 4.6,
+            size: '21 GB',
+            
+        },
+        {
+            title: 'WWE 2K18',
+            thumbnail: require('../../../assets/sr.jpg'),
+            subtitle: 'for PC',
+            rating: 4.5,
+            size: '32 GB',
+        },
+        {
+            title: 'FIFA 18',
+            thumbnail: require('../../../assets/games.jpg'),
+            subtitle: 'for PC',
+            rating: 4.6,
+            size: '21 GB',
+            
+        },
+        {
+            title: 'WWE 2K18',
+            thumbnail: require('../../../assets/sr.jpg'),
+            subtitle: 'for PC',
+            rating: 4.5,
+            size: '32 GB',
+        },
+    ]};
+    
+    renderItems() {
+        return this.state.items.map((item) => {
+        return (
+            <ItemCard 
+                    key={item.title}
+                    item={item}
+                    rightIconName='heart'
+                />
+        );
+        });
+    }
+    
     render(){
         const item = this.props.navigation.getParam('itemList');
         return(
@@ -21,8 +78,73 @@ export default class ItemListScreen extends Component{
                             leftIconName='ios-arrow-back'
                             onLeftButtonPress={() => this.props.navigation.goBack()}
                             headerTitle={item.title}
-                        />
+                />
+
+                <View style={styles.headerViewContainer}>
+                    <View style={styles.headerLeftView} >
+                        <TouchableOpacity activeOpacity={0.9}
+                        //onPress
+                        >
+                            <Icon name='ios-list' size={40} color={'black'} />
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.headerRightViews} >
+                        <TouchableOpacity activeOpacity={0.9}
+                        //onPress
+                        >
+                            <Icon name='logo-tux' size={35} color={'black'} flexDirection='row' >
+                            <Text al> Sort </Text>
+                            </Icon>
+                        </TouchableOpacity>
+                    </View>
+
+                    <View style={styles.headerRightViews} >
+                        <TouchableOpacity activeOpacity={0.9}
+                        //onPress
+                        >
+                            <Icon name='logo-reddit' size={35} color={'black'} flexDirection='row'>
+                            <Text> Filter </Text>
+                            </Icon>
+                        </TouchableOpacity>
+                    </View>
+
+                </View>
+                <Content >
+                    <SafeAreaView style={{ flex: 1 }}>
+                        <ScrollView>
+                            {this.renderItems()}
+                        </ScrollView>
+                    </SafeAreaView>
+                </Content>
             </Container>
         );
+    }
+    //<Icon name='ios-grid' size={40} style={{textAlign: "center"}} color={'black'} />
+}
+export default withNavigation(ItemListScreen);
+const styles= {
+    headerViewContainer:{
+        flexDirection: 'row',
+        borderBottomColor: '#D3D3D3',
+        marginBottom: 3,
+        height: 45
+    },
+    headerLeftView:{
+        flex: 1,
+        height:45,
+        borderBottomWidth: 0.5,
+        borderRightWidth: 0.5,
+        borderRightColor: '#D3D3D3',
+        alignItems: 'center'
+    },
+    headerRightViews:{
+        height:45,
+        borderRightColor: '#D3D3D3',
+        borderBottomWidth: 0.5,
+        borderRightWidth: 0.5,
+        flex: 2,
+        flexDirection: 'row',
+        alignItems: 'center'
     }
 }
